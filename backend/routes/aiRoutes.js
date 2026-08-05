8172c9dad4aeae0364e21dbc957ccc570f5ced70
tanishakandari02-ui <tanishakandari02@gmail.com>
2026-08-05 20:59:28 +0530

    Add AI image disease detection feature

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { GoogleGenAI } = require("@google/genai");
+const multer = require("multer");
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
 
-router.post("/disease", async (req, res) => {
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024
+  }
+});
+
+router.post("/disease", upload.single("image"), async (req, res) => {
   try {
     const { crop, symptoms } = req.body;
 
@@ -33,16 +41,31 @@ Provide:
 
     console.log("Using model:", "gemini-flash-latest");
 
-    const response = await ai.models.generateContent({
-      model: "gemini-flash-latest",
-      contents: prompt,
-    });
+     let contents = [
+  {
+    text: prompt
+  }
+];
+
+if (req.file) {
+  contents.push({
+    inlineData: {
+      data: req.file.buffer.toString("base64"),
+      mimeType: req.file.mimetype
+    }
+  });
+}
+
+const response = await ai.models.generateContent({
+  model: "gemini-flash-latest",
+  contents: contents,
+});
 
     console.log("Gemini Response:", response);
 
-    res.json({
-      result: response.text,
-    });
+   res.json({
+  result: response.text || "No response generated",
+});
 
   } catch (error) {
     console.error("FULL ERROR:", error);
